ddfbbc47e121db8bb6e35413f2fbd8406c5c951a
Finnn16 <harfintaufiq@gmail.com>
2025-02-13 23:08:07 +0700

    Navbar, Head MSG, Infinite Scroll Brand

---
+++ src/scripts/index.js
@@ -0,0 +1,5 @@
+// Bootstrap JS
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+const copy = document.querySelector(".logos-slide").cloneNode(true);
+document.querySelector(".logos").appendChild(copy);
